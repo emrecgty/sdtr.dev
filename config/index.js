ee7cc8f2ea08186f617d66fff395a42e2a8abfe0
Damla Köksal <me@damlakoksal.com>
2021-02-26 15:34:46 +0300

    fix: quick fix

--- config/index.js
+++ config/index.js
@@ -5,7 +5,8 @@ module.exports = {
   siteShortTitle: "SDTR", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "Yazılımcı, tasarımcı ve yetenekli insanların buluştuğu kocaman bir aile!",
-  siteUrl: "http://localhost:8000",
+  siteUrl: "https://brave-chandrasekhar-7e5578.netlify.app",
+  // TODO: SITEYI DEGISTIR
   siteLanguage: "tr_TR",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
   seoTitleSuffix: "SDTR", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
