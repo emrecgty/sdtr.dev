85f9384857b04431baac3202ff716cb00272870d
Damla Köksal <me@damlakoksal.com>
2021-02-27 21:57:06 +0300

    fix: change website links

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   siteShortTitle: "SDTR", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "Yazılımcı, tasarımcı ve yetenekli insanların buluştuğu kocaman bir aile!",
-  siteUrl: "https://sdtr.netlify.app",
+  siteUrl: "https://sdtr.dev",
   // TODO: SITEYI DEGISTIR
   siteLanguage: "tr_TR",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
